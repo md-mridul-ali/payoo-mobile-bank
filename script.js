d70217d61d82b26eb30a8fc71238b0bbc706d631
MD.MRIDUL <mridulaiub17@gmail.com>
2025-08-23 03:27:55 +0600

    added home.html file

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("connected")
+// login button functionality
+document.getElementById('loginButton')
+.addEventListener('click', function(e){
+    e.preventDefault();//button er by default behaviour ke prevent korbe
+    const mobileNumber= 12345678910;
+    const pinNumber=1234;
+    const mobileNumberValue=parseInt(document.getElementById('mobile-number').value);
+    const pinNumberValue=parseInt(document.getElementById('pin-number').value);
+    if(mobileNumberValue === mobileNumber && pinNumberValue === pinNumber)
+    {
+        window.location.href="./home.html";
+    }
+    else{
+        alert('mobile number or pin number invalid');
+    }
+    
+})
